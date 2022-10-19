1008417dd00c5ed00b50a2a8c22ff84257de8f4e
SaltSith66 <SaltSith66@gmail.com>
2022-10-19 23:01:09 +0200

    fix bugs in pb_helper, change source/destination message pointer type to MonitorMsg insted of void *,  remove unused memory allocation

--- pb_helper/pb_helper.c
+++ pb_helper/pb_helper.c
@@ -1,6 +1,6 @@
 #include "pb_helper.h"
 
-#include "protos_src/monitor.pb.h"
+
 #include "protos_src/util.pb.h"
 
 #include "nanopb/pb_encode.h"
@@ -12,39 +12,30 @@
 #include <stdlib.h>
 
 int
-pb_helper_encode_msg(void *in_msg, uint8_t *out_msg, uint32_t out_msg_len)
+pb_helper_encode_msg(monitor_MonitorMsg *in_msg, uint8_t *out_buff, uint32_t out_buff_size)
 {
-    if ((in_msg == NULL) || (out_msg == NULL)) {
+    if ((in_msg == NULL) || (out_buff == NULL)) {
         return -1;
     }
 
-    monitor_MonitorMsg *monitor_msg = (monitor_MonitorMsg *)in_msg;
-    uint8_t *proto_buf = out_msg;
-
-    pb_ostream_t stream = pb_ostream_from_buffer(proto_buf, (sizeof(monitor_MonitorMsg)));
-    bool result = pb_encode(&stream, monitor_MonitorMsg_fields, monitor_msg);
+    pb_ostream_t stream = pb_ostream_from_buffer(out_buff, out_buff_size);
+    bool result = pb_encode(&stream, monitor_MonitorMsg_fields, in_msg);
     assert(result == true);
 
-    free(monitor_msg);
+    free(in_msg);
 
     return stream.bytes_written;
 }
 
 int
-pb_helper_decode_msg(void *in_msg, uint32_t in_msg_len, void *out_msg, uint32_t out_msg_len)
+pb_helper_decode_msg(uint8_t *in_buff, uint32_t in_buff_size, monitor_MonitorMsg *out_msg)
 {
-    if ((in_msg == NULL) || (out_msg == NULL)) {
+    if ((in_buff == NULL) || (out_msg == NULL)) {
         return -1;
     }
 
-    uint8_t *proto_buf = malloc(sizeof(monitor_MonitorMsg));
-    assert(proto_buf != NULL);
-
-    monitor_MonitorMsg *monitor_msg = malloc(sizeof(monitor_MonitorMsg));
-    assert(monitor_msg != NULL);
-
-    pb_istream_t stream = pb_istream_from_buffer(in_msg, in_msg_len);
-    bool result = pb_decode(&stream, monitor_MonitorMsg_fields, monitor_msg);
+    pb_istream_t stream = pb_istream_from_buffer(in_buff, in_buff_size);
+    bool result = pb_decode(&stream, monitor_MonitorMsg_fields, out_msg);
     assert(result == true);
 
     return 0;
--- pb_helper/pb_helper.h
+++ pb_helper/pb_helper.h
@@ -2,5 +2,7 @@
 
 #include <stdint.h>
 
-int pb_helper_encode_msg(void *in_msg, uint8_t *out_msg, uint32_t out_msg_len);
-int pb_helper_decode_msg(void *in_msg, uint32_t in_msg_len, void *out_msg, uint32_t out_msg_len);
+#include "protos_src/monitor.pb.h"
+
+int pb_helper_encode_msg(monitor_MonitorMsg *in_msg, uint8_t *out_buff, uint32_t out_buff_size);
+int pb_helper_decode_msg(uint8_t *in_buff, uint32_t in_buff_size, monitor_MonitorMsg *out_msg);
